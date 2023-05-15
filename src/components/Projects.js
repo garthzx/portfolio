09e25d7085329b1de347c710e169a5d4c6ee8b23
Garth <gotfangarth@gmail.com>
2023-05-16 01:09:28 +0800

    implement renderProjectTiles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../styles/Projects.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { projectData } from "../data/project";
 
 const Projects = () => {
   useEffect(() => {
@@ -12,6 +13,34 @@ const Projects = () => {
 
   const listItemClickHandler = (e, menuName) => {
     setListItemClicked(menuName);
+  };
+
+  const renderProjectTiles = () => {
+    return projectData.map((project) => {
+      return <div className="project-tile" data-aos="fade-up">
+        <div className="project-tile-expansion">
+          <p className="project-description">{project.description}</p>
+        </div>
+        <div>
+          <img
+            src={project.imgSrc}
+            alt="survey-form"
+          />
+          <div className="tech-used">
+            {project.techUsed.map(i => <span>{i}</span>)}
+            <a
+              href={project.githubHref}
+              className="project-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+          <h2 className="project-title">{project.title}</h2>
+        </div>
+      </div>;
+    });
   };
 
   const renderMenuList = () => {
@@ -42,125 +71,7 @@ const Projects = () => {
         {/* <ul className="portfolio-menu">{renderMenuList()}</ul> */}
         <div className="project-list">
           <div className="grid-container">
-            <div className="project-tile" data-aos="fade-up">
-              <div className="project-tile-expansion">
-                <p className="project-description">
-                  Doodle is a simple search engine that is similar in features
-                  as Google's. It stores sites and images in a MySQL database.
-                </p>
-              </div>
-              <div>
-                <img
-                  src={require("../assets/projects/doodle.png")}
-                  alt="survey-form"
-                />
-                <div className="tech-used">
-                  <span>HTML5</span>
-                  <span>CSS3</span>
-                  <span>JavaScript</span>
-                  <span>PHP</span>
-                  <span>MYSQL</span>
-                  <a
-                    href="https://github.com/garthzx/doodle"
-                    className="project-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
-                <h2 className="project-title">Doodle</h2>
-              </div>
-            </div>
-            <div className="project-tile" data-aos="fade-up">
-              <div className="project-tile-expansion">
-                <p className="project-description">
-                  A dynamically-typed, C-based syntax programming language.
-                  Supports arithmetic operations, conditional statements, and
-                  loops. Written in Python 3.
-                </p>
-              </div>
-              <div>
-                <img
-                  style={{ objectFit: "contain" }}
-                  src={require("../assets/projects/gelic.png")}
-                  alt="tribute-page"
-                />
-                <div className="tech-used">
-                  <span>Python</span>
-                  <a
-                    href="https://github.com/garthzx/gelic"
-                    className="project-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
-                <h2 className="project-title">Gelic</h2>
-              </div>
-            </div>
-            <div className="project-tile" data-aos="fade-up">
-              <div className="project-tile-expansion">
-                <p className="project-description">
-                  An expense tracker application written in ASP.NET Core MVC,
-                  Bootstrap 5, and Syncfusion.
-                </p>
-              </div>
-              <div>
-                <img
-                  style={{ objectFit: "cover" }}
-                  src={require("../assets/projects/extrack.png")}
-                  alt="tribute-page"
-                />
-                <div className="tech-used">
-                  <span>ASP.NET MVC</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>Bootstrap</span>
-                  <span>Syncfusion</span>
-                  <a
-                    href="https://github.com/garthzx/extrack"
-                    className="project-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
-                <h2 className="project-title">ExTrack</h2>
-              </div>
-            </div>
-            <div className="project-tile" data-aos="fade-up">
-              <div className="project-tile-expansion">
-                <p className="project-description">
-                  A simple E-Commerce website built using ASP.NET Core MVC (.NET
-                  6). This was developed by following my online course on Udemy.
-                </p>
-              </div>
-              <div>
-                <img
-                  style={{ objectFit: "cover" }}
-                  src={require("../assets/projects/bulkybook.png")}
-                  alt="tribute-page"
-                />
-                <div className="tech-used">
-                  <span>ASP.NET MVC</span>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>Bootstrap</span>
-                  <a
-                    href="https://github.com/garthzx/bulkybook"
-                    className="project-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </div>
-                <h2 className="project-title">Bulky Book</h2>
-              </div>
-            </div>
+            {renderProjectTiles()}
           </div>
         </div>
       </div>
---
+++ src/data/project.js
@@ -0,0 +1,34 @@
+export const projectData = [
+  {
+    title: "Doodle",
+    description:
+      "Doodle is a simple search engine that is similar in features as Google's. It stores sites and images in a MySQL database.",
+    imgSrc: require("../assets/projects/doodle.png"),
+    techUsed: ["HTML5", "CSS3", "JavaScript", "PHP", "MYSQL"],
+    githubHref: "https://github.com/garthzx/doodle",
+  },
+  {
+    title: "Gelic",
+    description:
+      "A dynamically-typed, C-based syntax programming language. Supports arithmetic operations, conditional statements, and loops. Written in Python 3.",
+    imgSrc: require("../assets/projects/gelic.png"),
+    techUsed: ["Python"],
+    githubHref: "https://github.com/garthzx/gelic",
+  },
+  {
+    title: "ExTrack",
+    description:
+      "An expense tracker application written in ASP.NET Core MVC, Bootstrap 5, and Syncfusion.",
+    imgSrc: require("../assets/projects/extrack.png"),
+    techUsed: ["ASP.NET MVC", "HTML", "CSS3", "Bootstrap", "Syncfusion"],
+    githubHref: "https://github.com/garthzx/extrack",
+  },
+  {
+    title: "Bulky Book",
+    description:
+      "A simple E-Commerce website built using ASP.NET Core MVC (.NET 6). This was developed by following my online course on Udemy.",
+    imgSrc: require("../assets/projects/doodle.png"),
+    techUsed: ["ASP.NET MVC", "HTML", "CSS3", "Bootstrap"],
+    githubHref: "https://github.com/garthzx/bulkybook",
+  },
+];
